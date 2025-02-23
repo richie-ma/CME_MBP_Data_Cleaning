34280cd8a4533843e2d99d9b175b85d328c7abc7
Richie Ma <ruchuan2@illinois.edu>
2025-02-23 16:43:18 -0600

    Update Trade_Summary.R

--- Trade_Summary.R
+++ Trade_Summary.R
@@ -1,40 +1,41 @@
 ## trade summary
 
 
-rm(list=ls())
 
-library(data.table)
-library(stringr)
-
-## batching
-name <- list.files(pattern = "")
-date <-  as.Date(substr(name,1,8), "%Y%m%d")
-
-for (i in 1:length(name)){
-  print(name[i])
-  MDP <- name[i]
-  input <- MDP
-  data <-  fread(input, header = F)[[1L]]
+trade_summary <- function(raw_data_path, date, ...){
   
-  if(date[i] < "2015-11-20"){
-    ## loop starts##  
+  date <- as.Date(date)
+  
+  if (class(date)!="Date"){
+   
+     stop("date should be in the format as YYYY-MM-DD.")
     
-    Index <- gsub("\001",",",data) # delete all "\001" for each tag and replace it with "," (comma)
+  }
+  
+  data <-  fread(raw_data_path, header = F, sep="\\", fill=TRUE)[[1L]]
+  
+  if(date < "2015-11-20"){
+    
+    Index <- str_subset(data, "\001269=2")
+    Index <- str_replace_all(Index, "\001",",") 
+    Trade <- str_replace_all(Index,",269=2,",",")
+    
+    rm(Index)
     rm(data)
     
     ## delete the tags that are not necessary
-    Index <- gsub("1128=([^,]*),9=([^,]*),35=([^,]*),49=([^,]*),34=([^,]*),","",Index)
-    Index <- gsub(",5799=([^,]*),",",",Index)
-    Index <- gsub(",268=([^,]*),",",", Index)
-    Index <- gsub(",279=([^,]*),",",",Index)
-    Index <- gsub(",22=([^,]*),",",",Index)
-    Index <- gsub(",48=([^,]*),",",",Index)
-    Index <- gsub(",273=([^,]*),",",",Index)
-    Index <- gsub(",274=([^,]*),",",",Index)
-    Index <- gsub(",451=([^,]*),",",",Index)
-    Index <- gsub(",1003=([^,]*),",",",Index)
-    Index <- gsub(",1020=([^,]*),",",",Index)
-    Index <- gsub(",277=([^,]*),",",",Index)
+    
+    Trade <-  str_replace_all(Trade, "1128=([^,]*),9=([^,]*),35=([^,]*),49=([^,]*),", "")
+    Trade <-  str_replace_all(Trade, ",5799=([^,]*),", ",")
+    Trade <-  str_replace_all(Trade, ",268=([^,]*),", ",")
+    Trade <-  str_replace_all(Trade, ",279=([^,]*),", ",")
+    Trade <-  str_replace_all(Trade, ",22=([^,]*),", ",")
+    Trade <-  str_replace_all(Trade, ",48=([^,]*),", ",")
+    Trade <-  str_replace_all(Trade, ",273=([^,]*),", ",")
+    Trade <-  str_replace_all(Trade, ",274=([^,]*),", ",")
+    Trade <-  str_replace_all(Trade, ",451=([^,]*),", ",")
+    Trade <-  str_replace_all(Trade, ",1020=([^,]*),", ",")
+    
     
     
     ## searching for the following tags, 
@@ -51,27 +52,23 @@ for (i in 1:length(name)){
     ## Trade summary pattern
     ## 75 60 269=2 55 270 271 346 5797 37711-trade id 37705 37 32
     
-    Trade <- str_subset(Index, "269=2")
-    Trade <- gsub(",269=2,",",",Trade)
-    rm(Index)
-    
     if(length(Trade)!=0){
       
       ## generate the trade data (without order flow)
-      trade <- str_match_all(Trade, "83=([^,]*),107=([^,]*),270=([^,]*),271=([^,]*),")
+      trade <- str_match_all(Trade, "83=([^,]*),107=([^,]*),270=([^,]*),271=([^,]*),1003=([^,]*),5797=([^,]*),")
       n_row <- sapply(trade, nrow)
-      trade.info <- unlist(str_extract_all(Trade,"52=([^,]*),75=([^,]*),"))
+      trade.info <- unlist(str_extract_all(Trade,"34=([^,]*),52=([^,]*),75=([^,]*),"))
       trade.info <- str_dup(trade.info, n_row)
       trade <- as.data.table(do.call(rbind, trade))[,-1]
-      names(trade)[c(1:4)] <- c("Seq","Code","PX","Size")
+      names(trade)[c(1:6)] <- c("Seq","Code","PX", "Size","TrdID","agg")
       
-      trade.info <- str_match_all(trade.info, "52=([^,]*),75=([^,]*),")
+      trade.info <- str_match_all(trade.info, "34=([^,]*),52=([^,]*),75=([^,]*),")
       trade.info <- as.data.table(do.call(rbind,trade.info))[,-1]
-      names(trade.info)[c(1:2)] <- c("Time","Date")
+      names(trade.info)[c(1:3)] <- c("MsgSeq","SendingTime","Date")
       
       trade.info$Date <- as.Date(trade.info$Date, "%Y%m%d")
       #Time <- paste(substr(trade.info$Time,1,8),substr(trade.info$Time,9,10),substr(trade.info$Time,11,12),
-                    #substr(trade.info$Time,13,14),substr(trade.info$Time,15,23),sep=".")
+      #substr(trade.info$Time,13,14),substr(trade.info$Time,15,23),sep=".")
       #trade.info$Time <- as.POSIXct(Time, tz="UTC", "%Y%m%d.%H.%M.%OS") ## original time is UTC
       #attr(trade.info$Time, "tzone") <- "America/Chicago" ## US central time
       #trade$MsgSeq <- as.numeric(trade$MsgSeq)
@@ -82,47 +79,127 @@ for (i in 1:length(name)){
       trade <- cbind(trade.info, trade)
       rm(trade.info)
       
-      ## how many contracts are transacted today
-      contract <- unique(trade$Code)
+      ###
       
-      ## save the transaction data of each single contract
-      for (j in 1:length(contract)) {
-        
-        print(contract[j])
-        
-        Trade.sf <- subset(trade, trade$Code==contract[j])
-        
-        if(dim(Trade.sf)[1]!=0){
-          
-          ## delete the file with empty size and create new folders to save data to each folders
-          ## create multiple folders to save the data
-          new_fol <- file.path(paste0("C:/Users/ruchuan2/Box/Corn/transaction","/",contract[j]))
-          if(!dir.exists(new_fol)){
-            dir.create(new_fol)
-          }
-          fname <- file.path(new_fol,
-                             paste0(name[i], "_",contract[j],".rda"))
-          save(Trade.sf, file = fname) ## save all contracts
-          
-        }
-      }
+      trade1 <- str_match_all(Trade, "83=([^,]*),107=([^,]*),270=([^,]*),271=([^,]*),1003=([^,]*),")
+      n_row <- sapply(trade1, nrow)
+      trade.info1 <- unlist(str_extract_all(Trade,"34=([^,]*),52=([^,]*),75=([^,]*),"))
+      trade.info1 <- str_dup(trade.info1, n_row)
+      trade1 <- as.data.table(do.call(rbind, trade1))[,-1]
+      names(trade1)[c(1:5)] <- c("Seq","Code","PX", "Size","TrdID")
+      
+      trade.info1 <- str_match_all(trade.info1, "34=([^,]*),52=([^,]*),75=([^,]*),")
+      trade.info1 <- as.data.table(do.call(rbind,trade.info1))[,-1]
+      names(trade.info1)[c(1:3)] <- c("MsgSeq","SendingTime","Date")
+      
+      trade.info1$Date <- as.Date(trade.info1$Date, "%Y%m%d")
+      #Time <- paste(substr(trade.info$Time,1,8),substr(trade.info$Time,9,10),substr(trade.info$Time,11,12),
+      #substr(trade.info$Time,13,14),substr(trade.info$Time,15,23),sep=".")
+      #trade.info$Time <- as.POSIXct(Time, tz="UTC", "%Y%m%d.%H.%M.%OS") ## original time is UTC
+      #attr(trade.info$Time, "tzone") <- "America/Chicago" ## US central time
+      #trade$MsgSeq <- as.numeric(trade$MsgSeq)
+      trade1$Seq <- as.numeric(trade1$Seq)
+      trade1$PX <- as.numeric(trade1$PX)
+      trade1$Size <- as.numeric(trade1$Size)
+      
+      trade1 <- cbind(trade.info1, trade1)
+      rm(trade.info1)
+      
+      trade1.1 <- trade[, -"agg"]
+      
+      trade1 <- rbind(trade1.1, trade1)
+      
+      trade1 <- unique(trade1)
+      
+      trade <- merge(trade1, trade[, .(Seq, Code, agg)], by=c("Seq", "Code"), all=TRUE)
+      setkey(trade, Code, Seq)
+      
+      rm(trade1.1, trade1)
+      
+      
+      trade2 <- str_match_all(Trade, "83=([^,]*),107=([^,]*),270=([^,]*),271=([^,]*),277=([^,]*),1003=([^,]*),5797=([^,]*),")
+      n_row <- sapply(trade2, nrow)
+      trade.info2 <- unlist(str_extract_all(Trade,"34=([^,]*),52=([^,]*),75=([^,]*),"))
+      trade.info2 <- str_dup(trade.info2, n_row)
+      trade2 <- as.data.table(do.call(rbind, trade2))[,-1]
+      names(trade2)[c(1:7)] <- c("Seq","Code","PX","Size","TrdCon","TrdID","agg")
+      
+      trade.info2 <- str_match_all(trade.info2, "34=([^,]*),52=([^,]*),75=([^,]*),")
+      trade.info2 <- as.data.table(do.call(rbind,trade.info2))[,-1]
+      names(trade.info2)[c(1:3)] <- c("MsgSeq","SendingTime","Date")
+      
+      trade.info2$Date <- as.Date(trade.info2$Date, "%Y%m%d")
+      #Time <- paste(substr(trade.info$Time,1,8),substr(trade.info$Time,9,10),substr(trade.info$Time,11,12),
+      #substr(trade.info$Time,13,14),substr(trade.info$Time,15,23),sep=".")
+      #trade.info$Time <- as.POSIXct(Time, tz="UTC", "%Y%m%d.%H.%M.%OS") ## original time is UTC
+      #attr(trade.info$Time, "tzone") <- "America/Chicago" ## US central time
+      #trade$MsgSeq <- as.numeric(trade$MsgSeq)
+      trade2$Seq <- as.numeric(trade2$Seq)
+      trade2$PX <- as.numeric(trade2$PX)
+      trade2$Size <- as.numeric(trade2$Size)
+      
+      trade2 <- cbind(trade.info2, trade2)
+      rm( trade.info2)
+      
+      ## special trades
+      
+      trade3 <- str_match_all(Trade, "83=([^,]*),107=([^,]*),270=([^,]*),271=([^,]*),277=([^,]*),1003=([^,]*),")
+      n_row <- sapply(trade3, nrow)
+      trade.info3 <- unlist(str_extract_all(Trade,"34=([^,]*),52=([^,]*),75=([^,]*),"))
+      trade.info3 <- str_dup(trade.info3, n_row)
+      trade3 <- as.data.table(do.call(rbind, trade3))[,-1]
+      names(trade3)[c(1:6)] <- c("Seq","Code","PX","Size","TrdCon","TrdID")
+      
+      trade.info3 <- str_match_all(trade.info3, "34=([^,]*),52=([^,]*),75=([^,]*),")
+      trade.info3 <- as.data.table(do.call(rbind,trade.info3))[,-1]
+      names(trade.info3)[c(1:3)] <- c("MsgSeq","SendingTime","Date")
+      
+      trade.info3$Date <- as.Date(trade.info3$Date, "%Y%m%d")
+      #Time <- paste(substr(trade.info$Time,1,8),substr(trade.info$Time,9,10),substr(trade.info$Time,11,12),
+      #substr(trade.info$Time,13,14),substr(trade.info$Time,15,23),sep=".")
+      #trade.info$Time <- as.POSIXct(Time, tz="UTC", "%Y%m%d.%H.%M.%OS") ## original time is UTC
+      #attr(trade.info$Time, "tzone") <- "America/Chicago" ## US central time
+      #trade$MsgSeq <- as.numeric(trade$MsgSeq)
+      trade3$Seq <- as.numeric(trade3$Seq)
+      trade3$PX <- as.numeric(trade3$PX)
+      trade3$Size <- as.numeric(trade3$Size)
+      
+      trade3 <- cbind(trade.info3, trade3)
+      rm(trade.info3)
+      
+      trade2.2 <- trade2[, -c("agg")]
+      
+      trade2_3_all <- rbind(trade2.2, trade3)
+      
+      trade2_3_all <- unique(trade2_3_all)
+      
+      trade2_3_all <- merge(trade2[, .(Code, Seq, agg)], trade2_3_all, by=c( "Code","Seq"), all=TRUE)
+      
+      setkey(trade2_3_all, Code, Seq)
+      
+      
+      setkey(trade, Code, Seq)
+      
+      trade <- rbind(trade, trade2_3_all, fill=TRUE)
+      
+      setkey(trade, Code, Seq)
+      setcolorder(trade, c("Date", "MsgSeq", "SendingTime", "Code", "Seq", "PX", "Size", "agg"))
+      
+     
     }
-  } else{
-    
-
+  }  else{
+  
     
     Index <- str_subset(data, "\001269=2")
     Index <- str_replace_all(Index, "\001",",") 
     Trade <- str_replace_all(Index,",269=2,",",")
     rm(Index)
     
-    # delete all "\001" for each tag and replace it with "," (comma)
     rm(data)
     
     ## delete the tags that are not necessary
     
     Trade <- str_replace_all(Trade, "1128=([^,]*),9=([^,]*),35=([^,]*),49=([^,]*),","")
-    Trade <- str_replace_all(Trade, ",52=([^,]*),",",") # delete the record time (not the transact time)
     Trade <- str_replace_all(Trade, ",5799=([^,]*),",",")
     Trade <- str_replace_all(Trade, ",268=([^,]*),",",")
     Trade <- str_replace_all(Trade, ",279=([^,]*),",",")
@@ -156,14 +233,14 @@ for (i in 1:length(name)){
       ## generate the trade data (without order flow)
       trade <- str_match_all(Trade, "55=([^,]*),83=([^,]*),270=([^,]*),271=([^,]*),346=([^,]*),5797=([^,]*)")
       n_row <- sapply(trade, nrow)
-      trade.info <- unlist(str_extract_all(Trade,"75=([^,]*),34=([^,]*),60=([^,]*),"))
+      trade.info <- unlist(str_extract_all(Trade,"75=([^,]*),34=([^,]*),52=([^,]*),60=([^,]*),"))
       trade.info <- str_dup(trade.info, n_row)
       trade <- as.data.table(do.call(rbind, trade))[,-1]
       names(trade)[c(1:6)] <- c("Code","Seq","PX","Size","Ord","agg")
       
-      trade.info <- str_match_all(trade.info, "75=([^,]*),34=([^,]*),60=([^,]*),")
+      trade.info <- str_match_all(trade.info, "75=([^,]*),34=([^,]*),52=([^,]*),60=([^,]*),")
       trade.info <- as.data.table(do.call(rbind,trade.info))[,-1]
-      names(trade.info)[c(1:3)] <- c("Date","MsgSeq", "Time")
+      names(trade.info)[c(1:4)] <- c("Date","MsgSeq","SendingTime", "TransactTime")
       
       trade.info$Date <- as.Date(trade.info$Date, "%Y%m%d")
       #Time <- paste(substr(trade.info$Time,1,8),substr(trade.info$Time,9,10),substr(trade.info$Time,11,12),
@@ -172,7 +249,7 @@ for (i in 1:length(name)){
       #attr(trade.info$Time, "tzone") <- "America/Chicago" ## US central time
       #trade$MsgSeq <- as.numeric(trade$MsgSeq)
       trade$Seq <- as.numeric(trade$Seq)
-
+      
       trade$PX <- as.numeric(trade$PX)
       trade$Size <- as.numeric(trade$Size)
       trade$Ord <- as.numeric(trade$Ord)
@@ -180,34 +257,16 @@ for (i in 1:length(name)){
       
       trade <- cbind(trade.info, trade)
       trade$MsgSeq <- as.numeric(trade$MsgSeq)
-      rm(Time, trade.info)
-      
-      ## how many contracts are transacted today
-      contract <- unique(trade$Code)
-      
       setkey(trade, Code, Seq)
-      
-      # save the results
-      
-      for (k in 1:length(contract)) {
-        
-        #print(contract[k])
-        
-        Trade.sf <- subset(trade, trade$Code==contract[k])
-        
-        if(dim(Trade.sf)[1]!=0){
-          ## delete the file with empty size and create new folders to save data to each folders
-          ## create multiple folders to save the data
-          new_fol <- file.path(paste0("saving path","/",contract[k]))
-          if(!dir.exists(new_fol)){
-            dir.create(new_fol)
-          }
-          fname <- file.path(new_fol,
-                             paste0("header",substr(name[i], 1,8),"_", "r","_",sample(100000,1, replace = F), "_",contract[k],".rda"))
-          save(Trade.sf, file = fname) ## save all contracts
-          
-        }
-      }
+      setcolorder(trade, c("Date", "MsgSeq", "SendingTime", "TransactTime","Code", "Seq", "PX", "Size","Ord", "agg"))
+    }    
+   
     }  
-  }
+  
+  results <- split(trade, by="Code")
+  cat("CME MDP 3.0 Trade Summary", "\n", 
+      "contracts:", names(results))
+  return(results)
 }
+
+
